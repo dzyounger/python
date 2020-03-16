18d9b45cc718dcf592ea65aff6c1d64ca8cb1447
Young <dzyoung123@163.com>
2020-03-16 23:36:43 +0800

    flappy_init

---
+++ code/playflappybird/first_try.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append("game/")
+import cv2
+import wrapped_flappy_bird as game
+
+game_state = game.GameState()
+# 创建实例
+do = [0, 1]
+image, reward, terminal = game_state.frame_step(do)
+# 将一个动作输入到游戏中，获得游戏返回的结果
+print (image.shape, reward, terminal)
+# 打印 image 的大小， reward 和 terminal 的值
